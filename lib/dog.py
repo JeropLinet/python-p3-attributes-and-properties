3af40e7e3c37f4b779355b17498ff63e357e738a
Linet <teweilinet@gmail.com>
2024-05-25 01:47:08 +0300

    attributed & properties

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,25 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self,name="Unknown",breed="Unknown"):
+     self.name=name
+     self.breed=breed
+    def name(self):
+        return self._name
+    
+    def set_name(self,name):
+        if isinstance(name,str) and 1<len(name)<25:
+            self._name=name
+        else:
+            print("Name must be string between 1 and 25 characters.")
+            self._name=None
+    def set_breed(self,breed):
+        if breed in APPROVED_BREEDS:
+            print(breed)
+            self.breed=breed
+
+        else:
+            print("The breed must be in the following list of dog breeds")
+            print(", ".join(APPROVED_BREEDS))
+            self._breed = None
+
--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,21 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self,name="",job=""):
+        self.name=name
+        self.job=job
+    def name(self):
+        return self._name
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()  
+        else:
+            print("Name must be a string between 1 and 25 characters.")
+    def job(self):
+        return self._job
+    def job(self,job):
+     if job in APPROVED_JOBS:
+        self._job = job
+     else:
+       print("The job must be in the following list of jobs:",APPROVED_JOBS)
+     pass
